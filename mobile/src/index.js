068290d2ad7ad8f57bf6f87326fc947f0d3f5f04
Mateus V. Farias <55674918+fariasmateuss@users.noreply.github.com>
2020-04-16 18:00:32 -0300

    application mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Routes from'./routes';
+
+const App = () => {
+  return (
+    <Routes />
+  );
+};
+
+export default App;
---
+++ mobile/src/services/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333'
+});
